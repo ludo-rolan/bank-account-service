1327c14864ede5bbf79a6e03ce22fd75aa310ecf
ludo-rolan <ekallejrl@gmail.com>
2022-11-20 07:28:30 +0100

    account type changed to string && account rest controller created

--- README.md
+++ README.md
@@ -4,5 +4,10 @@
 <br/>
 <h5>Login</h5>
 <img src="screenshots/login.png"/>
+<br/>
 <h5>La base de données</h5>
 <img src="screenshots/database.png"/>
+<br/>
+<h3>Récupération des comptes existants</h3>
+<img src="screenshots/get_accounts_operation.png"/>
+<br/>
--- src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
+++ src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import org.sid.bankaccountservice.enums.AccountType;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import java.util.Date;
 
@@ -18,6 +20,7 @@ public class BankAccount {
     private Date createdAt;
     private double balance;
     private String currency;
+    @Enumerated(EnumType.STRING)
     private AccountType type;
 
 
