4d41375226645de0c0f1a0ad26974f8126e82c06
ludo-rolan <ekallejrl@gmail.com>
2022-11-20 22:03:40 +0100

    crud operations created on web service

--- README.md
+++ README.md
@@ -11,3 +11,28 @@
 <h3>Récupération des comptes existants</h3>
 <img src="screenshots/get_accounts_operation.png"/>
 <br/>
+<h3>Récupération d'un compte existant</h3>
+<br/>
+<h5>Url de récupération d'un compte</h5>
+<img src="screenshots/get_account_url.png"/>
+<br/>
+<h5>Résultat de la récupération du compte</h5>
+<img src="screenshots/get_account_operation.png"/>
+<br/>
+<h3>Consommation du web service Rest avec Postman</h3>
+<br/>
+<h5>Récupération des comptes existants</h5>
+<img src="screenshots/get_accounts_postman.png"/>
+<br/>
+<h5>Récupération d'un compte existant</h5>
+<img src="screenshots/get_account_postman.png"/>
+<br/>
+<h5>Création d'un compte avec Postman</h5>
+<img src="screenshots/create_account_postman.png"/>
+<br>
+<h5>Modification d'un compte existant avec Postman</h5>
+<img src="screenshots/update_account_postman.png"/>
+<br>
+<h5>Suppression d'un compte existant avec Postman</h5>
+<img src="screenshots/delete_account_postman.png"/>
+<br>
--- src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
+++ src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
@@ -2,11 +2,11 @@ package org.sid.bankaccountservice.web;
 
 import org.sid.bankaccountservice.entities.BankAccount;
 import org.sid.bankaccountservice.repositories.BankAccountRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class AccountRestController {
@@ -26,6 +26,28 @@ public class AccountRestController {
     public BankAccount bankAccount (@PathVariable String id) {
          return bankAccountRepository.findById(id)
                  .orElseThrow(()->new RuntimeException(String.format("Account %s not found", id)));
+    }
+
+    @PostMapping("/bankAccounts")
+    public BankAccount save (@RequestBody BankAccount bankAccount) {
+        if (bankAccount.getId() == null) bankAccount.setId(UUID.randomUUID().toString());
+        bankAccount.setCreatedAt(new Date());
+        return bankAccountRepository.save(bankAccount);
+    }
+
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update (@PathVariable String id, @RequestBody BankAccount bankAccount) {
+        BankAccount account = bankAccountRepository.findById(id).orElseThrow();
+        if (bankAccount.getBalance() != null) account.setBalance(bankAccount.getBalance());
+        if (bankAccount.getCreatedAt() != null) account.setCreatedAt(new Date());
+        if (bankAccount.getType() != null) account.setType(bankAccount.getType());
+        if (bankAccount.getCurrency() != null) account.setCurrency(bankAccount.getCurrency());
+        return bankAccountRepository.save(account);
+    }
+
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount (@PathVariable String id) {
+        bankAccountRepository.deleteById(id);
     }
 
 }
